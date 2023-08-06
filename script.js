0d68a25a8ba731e59f31f436fc40af9691fb0797
CryptoBug <98199611+cryptobugnft@users.noreply.github.com>
2023-08-06 09:11:09 +0800

    quick fix

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ async function loadWalletInfo() {
   // Initilize address and display QR code
   const regularAddr = await wallet.getDepositAddress();
   const tokenAddr = await wallet.getTokenDepositAddress();
-  const explorerAddr = "https://explorer.salemkode.com/address/" + regularAddr.replace("bitcoincash:","");
+  const explorerAddr = regularAddr.replace("bitcoincash:","");
   document.querySelector('#depositAddr').innerText = regularAddr;
   document.querySelector('#depositTokenAddr').innerText = tokenAddr;
   document.querySelector('#explorerAddr').innerText = explorerAddr;
